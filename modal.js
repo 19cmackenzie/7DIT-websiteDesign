dc976554eda61227cedc0c150999740f853b7fd7
CmacKenzie001 <104046376+CmacKenzie001@users.noreply.github.com>
2022-08-15 12:32:31 +1200

    Modal works for multiple images. code optimised.

--- modal.js
+++ modal.js
@@ -1,20 +1,21 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var img = document.getElementsByClassName('modal_trigger');
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("modal-img");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("galleryimg1");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+//put the image data of the clicked image into the modal div
+var showModal = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener('click', showModal);
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+//close modal
+span.onclick = function() { 
+    modal.style.display = "none";
 }
